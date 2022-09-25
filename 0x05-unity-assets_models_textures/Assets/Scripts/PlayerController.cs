cccc1239e402e4ab8ba18d0d44ffcc758bb89039
Soly-M-27 <solymar227@icloud.com>
2022-09-25 17:11:12 -0400

    Finished creating Player Controller and setting up Camera offset

--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -4,15 +4,86 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed;
+    public float jumpForce;
+
+    Rigidbody rb;  
+
+    private CharacterController characterController;
+
+    // Force Player to come back down by applying gravity 
+    // to the player each frame. Increase jump when button is pressed
+    // and decrease as the player comes back down so double jump doesn't
+    // take place
+    private float ySpeed;
+    
+    // Private field to fix Player StepOffset. Set it to 0
+    // when character is on the ground to avoid jerky jumping;
+    private float originalStepOffset;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>();
+        originalStepOffset = characterController.stepOffset;
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float H = Input.GetAxis("Horizontal");
+        float V = Input.GetAxis("Vertical");
+
+        Vector3 moveDir = new Vector3(H, 0, V);
+        float magnitude = Mathf.Clamp01(moveDir.magnitude) * speed;
+        moveDir.Normalize();
+
+        // Adjust the gravity. We're getting the physics gravity value
+        // and adding this amount to our y speed every second.
+        // Time.delatTime is the amount of seconds that have passed since the
+        // last frame. By default the physics gravity value is set to
+        // -9.81 in a Y direction. So this line will decrease our Y Speed
+        // by 9.81 every second.
+        ySpeed += Physics.gravity.y * Time.deltaTime;
+
+        // Freeze Cylinder in place to avoid floating rotations 
+        rb.constraints = RigidbodyConstraints.FreezeRotation;
+
+        // Now we need to use our ySpeed when moving the character. 
+        // To do this we need to extract the calculation we pass into
+        // the SimpleMove method and instead assign it to variable.
+
+        Vector3 velocity = moveDir * magnitude;
+        velocity.y = ySpeed;
+
+        // SimpleMove is only for simple movement and ignores any
+        // movement in the Y direction. Switch to Move Method.
+        // You must multiply by Time.deltaTime to ensure the characeter
+        // moves at the same speed regardless of the frame rate
+        characterController.Move(velocity * Time.deltaTime);
+
+        if (characterController.isGrounded) // isGrounded is a Flag
+        {
+            // When character is not on the ground, set OG StepOffset
+            characterController.stepOffset = originalStepOffset;
+
+            // Set ySpeed back to a negative amount to stop it from 
+            // increasing indefinitely. If set to zero, Jump button
+            // will only work once.
+            ySpeed = -0.5f;
+
+            // Check if player is pressing the jump button
+            if (Input.GetKey(KeyCode.Space))
+            {
+                ySpeed = jumpForce;
+                rb.constraints = RigidbodyConstraints.FreezeRotation;
+            }
+        }
+        else
+        {
+            // Sets it to 0 when character is on the ground;
+            characterController.stepOffset = 0;
+        }
     }
 }
